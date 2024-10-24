c1b114012048d80ed36bade0784305eb3f2bd77d
harshpa324 <harshpa324@gmail.com>
2024-10-24 20:19:59 +0530

    Finished User profile Dropdown and perfected auth flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
+  authModal
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  authModal: React.ReactNode
 }) {
   return (
     <html
@@ -23,9 +25,11 @@ export default function RootLayout({
         inter.className
       )}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-      <Navbar />
+        {/* @ts-expect-error server component */}
+        <Navbar />
+        {authModal}
+
         <div className='container max-w-7xl mx-auto h-full pt-12'>
-          
           {children}
         </div>
         <Toaster />
